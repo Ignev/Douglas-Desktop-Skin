84d507e154373822787461d276f040b432dcda2a
Evgeny Belov <belov.zhenya-belov1999@yandex.ru>
2021-02-18 18:21:42 +0300

    d

--- left/js/script.js
+++ left/js/script.js
@@ -39,18 +39,109 @@ function openPopup(openBtnSelector, popupSelector) {
     popup.style.animation = "fadeIn 1s ease";
   });
 }
+let count = 1;
+console.log(count);
+
 closePopup("#close1", "#popup1");
 closePopup("#close2", "#popup2");
 closePopup("#close3", "#popup3");
 openPopup("#plus1", "#popup1");
 openPopup("#plus2", "#popup2");
 openPopup("#plus3", "#popup3");
+
 $(".slider").on(
   "afterChange",
   function (event, slick, currentSlide, nextSlide) {
+    count++;
+    const womanOne = document.querySelector("#top-woman-1");
+    const womanTwo = document.querySelector("#top-woman-2");
+    const womanThree = document.querySelector("#top-woman-3");
+    const BottomWomanOne = document.querySelector("#bottom-woman-1");
+    const BottomWomanTwo = document.querySelector("#bottom-woman-2");
+    const BottomWomanThree = document.querySelector("#bottom-woman-3");
+
+if(count <= 3){
+      if(count == 2){
+        BottomWomanOne.style.animation = "";
+        womanOne.style.animation = "";
+        BottomWomanTwo.style.opacity = "1"
+        womanTwo.style.opacity = "1"
+        BottomWomanThree.style.animation = "fadeBg 1s ease";
+        womanThree.style.animation = "fadeBg 1s ease";
+        setTimeout(() => {
+          womanThree.style.opacity = "0";
+          BottomWomanThree.style.opacity = "0";
+        }, 1000)
+      }
+      if(count == 3){
+        BottomWomanOne.style.opacity = "1"
+        womanOne.style.opacity = "1"
+        BottomWomanTwo.style.animation = "fadeBg 1s ease";
+        womanTwo.style.animation = "fadeBg 1s ease";
+        setTimeout(() => {
+          BottomWomanTwo.style.opacity = "0";
+          womanTwo.style.opacity = "0";
+        }, 1000)
+      }
+    }
+    else{
+      count = 1;
+      womanOne.style.animation = "fadeBg 1s ease";
+      BottomWomanOne.style.animation = "fadeBg 1s ease";
+      
+        setTimeout(() => {
+          womanOne.style.opacity = "0";
+          BottomWomanOne.style.opacity = "0";
+          womanThree.style.animation = "";
+          womanThree.style.opacity = "1";
+      BottomWomanThree.style.opacity = "1";
+          BottomWomanThree.style.animation = "";
+          womanTwo.style.animation = "";
+          BottomWomanTwo.style.animation = "";
+        }, 1000)
+    }
     const popups = document.querySelectorAll(".popup");
     popups.forEach((item) => {
       item.style.display = "none";
     });
   }
 );
+
+
+/*
+    if(count > 2){
+      count = 0;
+    }
+    const topWoman = document.querySelectorAll(".woman");
+    const bottomWoman = document.querySelectorAll(".woman-bottom");
+    for (let i = 0; i < topWoman.length; i++) {
+      if(count == 0){
+        topWoman[2].style.animation = "fadeBg 1s ease";
+        console.log(count);
+      }
+      else{
+        topWoman[count-1].style.animation = "fadeBg 1s ease";
+        console.log(count);
+      }
+      
+      setTimeout(() => {
+        topWoman[count].classList.add("woman-actvie");
+        if(count == 0){
+          topWoman[2].style.animation = "fadeBg 1s ease";
+          topWoman[2].classList.remove("woman-actvie");
+          topWoman[2].style.opacity = "0";
+          topWoman[2].style.zIndex = "1";
+        }
+        else{
+          topWoman[count-1].classList.remove("woman-actvie");
+          topWoman[count-1].style.opacity = "0";
+          topWoman[count-1].style.zIndex = "1";
+        }
+       
+      }, 1000);
+    }
+    for (let j = 0; j < bottomWoman.length; j++) {
+      
+    }
+    console.log(count);
+*/
